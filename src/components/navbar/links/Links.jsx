746c8e1c78120006c1437710d046e50fd8c13cfe
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-02-22 02:04:56 +0000

    Nextjs

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,28 @@
+const Links = () => {
+    const links = [
+        {
+            title: "Homepage",
+            path: "/",
+        },
+        {
+            title: "About",
+            path: "/about",
+        },
+        {
+            title: "Contact",
+            path: "/contact",
+        },
+        {
+            title: "Blog",
+            path: "/posts",
+        },
+    ]
+
+
+
+return (
+<div></div>
+)
+}
+export default Links
+
